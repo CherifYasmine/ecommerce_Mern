c2e23649fb6b5e2aba1c44bbc10e2d3ea08726fb
CherifYasmine <yasmine.cherif9@gmail.com>
2020-12-31 11:24:29 +0100

    Header and footer Components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,17 @@
 import React from "react";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 const App = () => {
   return (
     <>
-      <h1>welcome</h1>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>welcome</h1>
+        </Container>
+      </main>
+      <Footer />
     </>
   );
 };
