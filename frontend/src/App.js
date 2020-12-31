389e2ad14234db766836b6341b0b0ded69725a83
CherifYasmine <yasmine.cherif9@gmail.com>
2020-12-31 13:02:11 +0100

    HomeScreen Component: listing products + Rating Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,14 @@ import React from "react";
 import { Container } from "react-bootstrap";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import HomeScreen from "./Screens/HomeScreen";
 const App = () => {
   return (
     <>
       <Header />
       <main className="py-3">
         <Container>
-          <h1>welcome</h1>
+          <HomeScreen />
         </Container>
       </main>
       <Footer />
